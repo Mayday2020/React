5c7115bb5474ee624a288b1d097706b246196d24
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2020-10-24 23:34:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+//import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <div className="header-icon"></div>
       </header>
+      <nav className="nav">
+        <ul className="sidebar">
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Messages</a></li>
+          <li><a href="#">Music</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Settings</a></li>
+        </ul>
+      </nav>
+      <div className="content">
+        <div className="content-img"></div>
+        <div className="avatar-img">
+          avatar + description
+        </div>
+        <div className="post">
+          my post
+          <div>
+            new post
+          </div>
+          <div>
+            posts
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
