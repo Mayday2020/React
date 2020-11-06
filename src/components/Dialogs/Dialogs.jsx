aad8d294b780cbdc2be283052010d1e38f736c00
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2020-11-06 19:40:35 +0300

    'lesson21_exact'

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,25 @@
 import s from './Dialogs.module.css';
-
+import {NavLink} from "react-router-dom";
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>Dmitry</div>
-                <div className={s.dialog}>Vlad</div>
-                <div className={s.dialog}>Kostya</div>
-                <div className={s.dialog}>Vova</div>
-                <div className={s.dialog}>Nastya</div>
+                <div className={s.dialog + ' ' + s.active}><NavLink to="/dialogs/1">Dmitry</NavLink></div>
+                <div className={s.dialog}><NavLink to="/dialogs/2">Vlad</NavLink></div>
+                <div className={s.dialog}><NavLink to="/dialogs/3">Kostya</NavLink></div>
+                <div className={s.dialog}><NavLink to="/dialogs/4">Vova</NavLink></div>
+                <div className={s.dialog}><NavLink to="/dialogs/5">Nastya</NavLink></div>
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi!</div>
-                <div className={s.message}>How are you?</div>
-                <div className={s.message}>Whera are you from?</div>
+                <div className={s.message}>
+                    Hi!
+                </div>
+                <div className={s.message}>
+                    How are you?
+                </div>
+                <div className={s.message}>
+                    Whera are you from?
+                </div>
             </div>
         </div>
     )
